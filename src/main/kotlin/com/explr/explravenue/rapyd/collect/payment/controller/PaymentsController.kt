d146b9d4198901238376d87a2025a0a4dddb7017
Lebogang Nkosi <lebogangnko@discovery.co.za>
2022-09-08 21:18:30 +0200

    Updated payment controller

--- src/main/kotlin/com/explr/explravenue/rapyd/collect/payment/controller/PaymentsController.kt
+++ src/main/kotlin/com/explr/explravenue/rapyd/collect/payment/controller/PaymentsController.kt
@@ -33,12 +33,12 @@ object PaymentsController {
     }
 
     @PostMapping(value = ["/bank"])
-    fun createBankPayment(@RequestBody request: CreateBankPaymentRequest): String {
+    fun createBankPayment(@RequestBody request: CreateBankPaymentRequest): CreateBankPaymentResponse {
         val restTemplate = RestTemplate()
         val entity = HttpEntity(Gson().toJson(request).replace(" ", ""), HeaderGenerator.generateRapydApiHeaders(Constants.POST_METHOD, PAYMENT_PATH, request))
         val response = restTemplate.exchange(Constants.BASE_URL + PAYMENT_PATH, HttpMethod.POST, entity, String::class.java)
         val type: Type = object : TypeToken<CreateBankPaymentResponse>() {}.type
-        return "Hello"
+        return Gson().fromJson(response.body, type)
     }
 
 }
