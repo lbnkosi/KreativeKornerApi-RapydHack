781db5f78c1710fe90fdfa49060f625677477ade
Lebogang Nkosi <lebogangnko@discovery.co.za>
2022-09-09 22:55:55 +0200

    Added response headers

--- src/main/kotlin/com/explr/explravenue/kreativeapi/accesspoint/AccessPointController.kt
+++ src/main/kotlin/com/explr/explravenue/kreativeapi/accesspoint/AccessPointController.kt
@@ -3,6 +3,7 @@ package com.explr.explravenue.kreativeapi.accesspoint
 import com.explr.explravenue.kreativeapi.accesspoint.Routes.CREATE_USER
 import com.explr.explravenue.kreativeapi.user.controller.UserController
 import com.google.gson.Gson
+import org.springframework.http.HttpHeaders
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
@@ -15,10 +16,14 @@ object AccessPointController {
 
     @PostMapping(value = ["/post"])
     fun acceptAccessPoint(@RequestBody request: AccessPointRequest): ResponseEntity<Any> {
+        val responseHeaders = HttpHeaders()
+        responseHeaders.set("Access-Control-Allow-Origin", "*")
+
+
         val response = route(request)
         val debug = Gson().toJson(response)
         println("DEBUG_PRINT - $debug")
-        return ResponseEntity.ok(response)
+        return ResponseEntity.ok().headers(responseHeaders).body(response)
     }
 
     private fun route(request: AccessPointRequest): Any {
